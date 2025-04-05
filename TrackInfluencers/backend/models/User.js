1c5f670d41dacb857fad68351833e0b49e6a30da
Krithika C <139158735+ckritk@users.noreply.github.com>
2025-04-06 00:31:08 +0530

    Delete TrackInfluencers/backend directory

--- TrackInfluencers/backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema({
-  username: { type: String, required: true },
-  campaigns: [{ type: Schema.Types.ObjectId, ref: 'Campaign' }],
-});
-
-module.exports = mongoose.model('User', userSchema);
